fe67bf23b158ca6e4ba9130d23097cb924b95755
Baris Burak Balci <barisburakbalci@gmail.com>
2023-09-15 22:46:04 +0300

    Created DbContext

---
+++ sales-forms/Data/FormDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sales_forms.Models;
+
+namespace sales_forms.Data
+{
+	public class FormDbContext : DbContext
+	{
+        public FormDbContext(DbContextOptions<FormDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Answer>().ToTable("Answer");
+            modelBuilder.Entity<Client>().ToTable("Client");
+            modelBuilder.Entity<Form>().ToTable("Form");
+            modelBuilder.Entity<Option>().ToTable("Option");
+            modelBuilder.Entity<Participant>().ToTable("Participant");
+            modelBuilder.Entity<Question>().ToTable("Question");
+        }
+    }
+}
+
--- sales-forms/Program.cs
+++ sales-forms/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using sales_forms.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<FormDbContext>(options => {
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("TarifDb");
+
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
