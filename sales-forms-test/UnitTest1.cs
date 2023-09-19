126ea7f47dec990ae5f93183ae40545a8eddae05
Barış Burak BALCI <barisburakbalci@gmail.com>
2023-09-19 23:28:33 +0300

    Created Unit Test project

---
+++ sales-forms-test/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace sales_forms_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CreateClient()
+        {
+
+            Assert.Pass();
+        }
+    }
+}
