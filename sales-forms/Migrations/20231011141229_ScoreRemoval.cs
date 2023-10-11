bde46ae5929a29b46e693250dd33ffb23d35f2fe
Barış Burak BALCI <barisburakbalci@gmail.com>
2023-10-11 17:14:56 +0300

    Removal of Score field from Participant table

---
+++ sales-forms/Migrations/20231011141229_ScoreRemoval.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sales_forms.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScoreRemoval : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "Participants");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "Participants",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
